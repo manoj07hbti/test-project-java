0667fc9bfee4adb09279b47283bdcff87097a006
60manojkumar@gmail.com <Motivated@2019>
2021-07-03 20:18:22 +0530

    Initial commit for core java Project

---
+++ src/Demo.java
@@ -0,0 +1,13 @@
+public class Demo {
+
+    //SYNTAX ?
+    public static void main(String[] args) {
+
+        System.out.println("Hello , This is my first java Program.... ");
+        System.out.println("I have created first program ");
+        System.out.println("hfhsfssasjasadkjsfd");
+    }
+
+}
+
+
