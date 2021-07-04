08c91c4ddec6cbba0e132520b6e262bf6fb4b9ae
60manojkumar@gmail.com <Motivated@2019>
2021-07-04 20:29:18 +0530

    Added Java Variables

---
+++ src/VariablesDemo.java
@@ -0,0 +1,47 @@
+public class VariablesDemo {
+
+
+
+    public static void main(String[] args) {
+
+        // SYNTAX : DataType variable_name = value ;
+        // you can not declare same name variable again but we can reuse
+
+        // String DataType
+        String name = "Rahul" ;
+        System.out.println(name);
+        name="Raj";
+        System.out.println(name);
+        String cityName="AGRA";
+        System.out.println(cityName);
+        String mobileBrand="SAMSUNG";
+        System.out.println(mobileBrand);
+
+        System.out.println("Printing Name: " +name);
+        System.out.println("Printing City Name: " +cityName);
+        System.out.println("Printing Mobile  Brand: " +mobileBrand);
+
+       // int datatype
+        // SYNTAX : DataType variable_name = value ;
+
+        int age=25;
+        System.out.println(age);
+        int marks=65;
+        System.out.println(marks);
+
+        // double datatype
+        // SYNTAX : DataType variable_name = value ;
+        double price = 41.3;
+        System.out.println(price);
+        double percentage=76.3;
+        System.out.println(percentage);
+
+        // boolean
+        boolean flag=false;
+        System.out.println(flag);
+
+
+
+    }
+
+}
