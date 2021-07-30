b603217a2f3b4dc84b5065256cf74343f3fefcde
60manojkumar@gmail.com <Motivated@2019>
2021-07-30 20:01:58 +0530

    Inheritance Day 1

---
+++ src/inheritance/A.java
@@ -0,0 +1,20 @@
+package inheritance;
+
+public class A {
+
+
+    public void m1(){
+
+        System.out.println("I am in M1 method of CLASS A..");
+    }
+
+    public void m2(){
+
+        System.out.println("I am in M2 method of CLASS A..");
+    }
+
+    public static void main(String[] args) {
+        A obj=new A();
+        obj.m1();
+    }
+}
