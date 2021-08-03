440064c3dcae3e17e546fe385dcb44cdc87e7505
60manojkumar@gmail.com <Motivated@2019>
2021-08-03 19:47:42 +0530

    Inheritance Day 3 with Abstract Keyword

---
+++ src/inheritance/AbstractMethodDemo.java
@@ -0,0 +1,17 @@
+package inheritance;
+
+public abstract class AbstractMethodDemo {
+
+    // a method without method body is called abstract method
+
+    public abstract void demo();
+
+    // a class with minimum one abstract method will be called as abstract class
+
+    public void m1(){
+
+        System.out.println("This is normal method");
+    }
+
+
+}
